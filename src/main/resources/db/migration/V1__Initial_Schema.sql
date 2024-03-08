94cd6dd0f3184f6daa350531995fed72ecc3bd37
Sean Welsh <s.welsh1314@gmail.com>
2024-03-08 16:04:34 -0500

    added database schema

--- src/main/resources/db/migration/V1__Initial_Schema.sql
+++ src/main/resources/db/migration/V1__Initial_Schema.sql
@@ -1,13 +1,46 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    user_name VARCHAR(50),
+    email VARCHAR(80) NOT NULL,
+    wins INT NOT NULL DEFAULT 0,
+    losses INT NOT NULL DEFAULT 0,
+    moves INT NOT NULL DEFAULT 0
+
 );
 
--- Create the game table
 CREATE TABLE games (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    moves INT NOT NULL DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    player_1 INT NOT NULL,
+    player_2 INT NOT NULL,
+    status VARCHAR(60),
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    FOREIGN KEY (player_1) REFERENCES users(id),
+    FOREIGN KEY (player_2) REFERENCES users(id)
 );
+
+CREATE TABLE pieces (
+    id SERIAL PRIMARY KEY,
+    color VARCHAR(5),
+    king BOOLEAN,
+    row INT,
+    col INT,
+    game_id INT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id)
+    );
+
+CREATE TABLE moves (
+    id SERIAL PRIMARY KEY,
+    game_id INT,
+    user_id INT,
+    piece_id INT,
+    source_row INT,
+    source_column INT,
+    destination_row INT,
+    destination_column INT,
+    moved_at TIMESTAMP,
+    FOREIGN KEY (game_id) REFERENCES games(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (piece_id) REFERENCES pieces(id)
+    );
