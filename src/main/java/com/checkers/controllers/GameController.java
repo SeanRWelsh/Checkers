d0250bc5e13326ee9174b7733da70c391ab25d5e
Sean Welsh <s.welsh1314@gmail.com>
2024-04-08 16:53:01 -0400

    removed unused imports

--- src/main/java/com/checkers/controllers/GameController.java
+++ src/main/java/com/checkers/controllers/GameController.java
@@ -2,11 +2,10 @@ package com.checkers.controllers;
 
 import com.checkers.dtos.GameDTO;
 import com.checkers.models.Game;
-import com.checkers.models.Player;
 import com.checkers.repositories.GameRepository;
-import com.checkers.repositories.PlayerRepository;
 import com.checkers.service.GameCreationService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,7 +33,7 @@ public class GameController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<GameDTO> createGame(@RequestBody CreateGameRequest createGameRequest,
+    public ResponseEntity<GameDTO> createGame(@RequestBody @NotNull CreateGameRequest createGameRequest,
             HttpServletRequest request) {
         Game newGame = gameCreationService.createGame(createGameRequest.player1_id, createGameRequest.player2_id);
         return ResponseEntity.status(HttpStatus.CREATED).body(new GameDTO(newGame));
