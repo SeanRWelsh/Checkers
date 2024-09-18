e7ba855cc7666c44868a0808d028c72898c2906c
Sean Welsh <s.welsh1314@gmail.com>
2024-09-17 20:47:00 -0400

    initial schema fixed

--- src/main/resources/db/migration/V10__adding_player_turn_to_games.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE games ADD COLUMN player_turn BIGINT;
-ALTER TABLE games ADD CONSTRAINT fk_player_turn FOREIGN KEY (player_turn) REFERENCES players(id);
-
-CREATE INDEX idx_games_player_turn ON games (player_turn);
--- src/main/resources/db/migration/V9__update_pieces_and_games.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE pieces ADD COLUMN player_id BIGINT NOT NULL;
-ALTER TABLE pieces ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES players(id);
-CREATE INDEX idx_pieces_player_id ON pieces (player_id);
-
-ALTER TABLE games ADD COLUMN winner_id BIGINT NULL;
-ALTER TABLE games ADD CONSTRAINT fk_winner_id FOREIGN KEY (winner_id) REFERENCES players(id);
-CREATE INDEX idx_games_winner_id ON games (winner_id);
