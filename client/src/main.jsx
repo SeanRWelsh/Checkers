4ecff1c28ea32a3b8afa3290d5608bf11a9808cc
Sean Welsh <s.welsh1314@gmail.com>
2024-10-06 15:18:12 -0400

    on refresh user stays logged in on the front end

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/index.css";
+import { UserProvider } from "./context/User";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </BrowserRouter>,
 );
