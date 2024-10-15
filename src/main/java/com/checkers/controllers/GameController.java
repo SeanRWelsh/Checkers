1f69c814082cc7bcdec4bbbe4b277edd0dea83c6
Sean Welsh <s.welsh1314@gmail.com>
2024-10-15 17:08:55 -0400

    resume game now restarts your most recent in progress game

--- src/main/java/com/checkers/controllers/GameController.java
+++ src/main/java/com/checkers/controllers/GameController.java
@@ -7,12 +7,15 @@ import com.checkers.repositories.GameRepository;
 import com.checkers.repositories.PlayerRepository;
 import com.checkers.securityConfiguration.SecurityUser;
 import com.checkers.service.GameService;
+
 import jakarta.validation.constraints.NotNull;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/game")
@@ -30,9 +33,12 @@ public class GameController {
     @GetMapping("/resume")
     public ResponseEntity<GameDTO> getGame(@AuthenticationPrincipal SecurityUser user) {
         Player player = playerRepository.findPlayerByUsername(user.getUsername());
-        Long gameId = gameRepository.findMostRecentGame(player.getId());
-        Game game = gameRepository.findById(gameId).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Game with id " + gameId + " not found."));
+        // Limit not in JPQL used pageable to limit return size to a List of 1. Could
+        // have done a native query to use limit but then would have to query for the
+        // game id then query for the game. also easy to modify this in the future to
+        // send back a list of games they may want to resume.
+        Pageable pageable = PageRequest.of(0, 1);
+        Game game = gameRepository.findMostRecentGame(player.getId(), pageable).get(0);
         return ResponseEntity.status(HttpStatus.OK).body(new GameDTO(game));
     }
 
